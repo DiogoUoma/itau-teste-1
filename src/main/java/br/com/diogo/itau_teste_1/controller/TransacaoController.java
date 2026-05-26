aef76fea27a535b12f1e66f6fd19a42d7b263629
DiogoUoma <diogouoma3105@gmail.com>
2026-05-26 11:44:58 -0300

    feat: Adicionado endpoint GET /estatistica

--- src/main/java/br/com/diogo/itau_teste_1/controller/TransacaoController.java
+++ src/main/java/br/com/diogo/itau_teste_1/controller/TransacaoController.java
@@ -4,33 +4,41 @@ import br.com.diogo.itau_teste_1.dto.EstatisticaResponse;
 import br.com.diogo.itau_teste_1.entity.TransacaoEntity;
 import br.com.diogo.itau_teste_1.service.TransacaoService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.DoubleSummaryStatistics;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
-@RequestMapping("/transacao")
 @AllArgsConstructor
 public class TransacaoController {
 
     private final TransacaoService transacaoService;
 
-    @PostMapping
+    @PostMapping("/transacao")
     public ResponseEntity<Void> novaTransacao(@RequestBody TransacaoEntity transacao) {
         transacaoService.salvarNovaTransacao(transacao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/transacao")
     public ResponseEntity<Void> deletaTransacoes() {
         transacaoService.limparTransacao();
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/estatistica")
+    public EstatisticaResponse calculaEstatisticas() {
+        DoubleSummaryStatistics doubleSummaryStatistics = transacaoService.calcularEstatisticas();
+
+        return new EstatisticaResponse(
+                doubleSummaryStatistics.getCount(),
+                doubleSummaryStatistics.getSum(),
+                doubleSummaryStatistics.getAverage(),
+                doubleSummaryStatistics.getCount() > 0 ? doubleSummaryStatistics.getMin() : 0.0,
+                doubleSummaryStatistics.getCount() > 0 ? doubleSummaryStatistics.getMax() : 0.0
+        );
+    }
 
 }
